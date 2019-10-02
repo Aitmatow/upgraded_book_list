fd435cb37e0572f7e93cdb9943d112fd2e7e2e60
Diiar <aitmarowd@gmail.com>
2019-10-02 11:44:35 +0600

    Добавил пагинацию.

--- source/webapp/views/comment_views.py
+++ source/webapp/views/comment_views.py
@@ -1,13 +1,19 @@
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+
 
 from webapp.models import Comment
+
 
 class CommentList(ListView):
     template_name = 'comments/comments_list.html'
     model = Comment
+
+    paginate_by = 5
+    paginate_orphans = 1
+    page_kwarg = 'page'
 
 class CommentDetail(DetailView):
     template_name = 'comments/comments_detail.html'
